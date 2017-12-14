5d777e669446a9c23f83cbe380be91293c551c5c
priyadharshini <kichan1655@gmail.com>
2017-12-14 12:57:03 +0530

    My projct setup

---
+++ Artbackend/src/main/java/Art/craft/Artbackend/App.java
@@ -0,0 +1,13 @@
+package Art.craft.Artbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
